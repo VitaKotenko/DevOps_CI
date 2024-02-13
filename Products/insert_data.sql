4c8ad44a3b0ab15d94eea2360a8d42efa3c05f35
Vita Kotenko <mialkovska.viktoria@gmail.com>
2024-02-13 14:32:38 +0200

    Initial commit

---
+++ Products/insert_data.sql
@@ -0,0 +1,8 @@
+USE Products;
+
+INSERT INTO [dbo].[Products] ([Name], [Price])
+VALUES ('Sugar', 32.0),
+       ('Salt', 19.0),
+       ('Bread', 20.0),
+       ('Butter', 62.0),
+       ('Milk', 32.0);
